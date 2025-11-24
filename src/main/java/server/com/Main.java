ec8a6af046b5139e04fb13796ff6213d3c699916
GavrisFlavius07 <gavris.flavius@itismeucci.com>
2025-11-24 10:30:46 +0100

    esercizio finito server http

---
+++ src/main/java/server/com/Main.java
@@ -0,0 +1,16 @@
+package server.com;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        ServerSocket ss = new ServerSocket(3000);     
+        while(true){
+            Socket s = ss.accept();
+            MyThread thread = new MyThread(s);
+            thread.start();
+        }
+    }
+}
---
+++ src/main/java/server/com/MyThread.java
@@ -0,0 +1,57 @@
+package server.com;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class MyThread extends Thread {
+    Socket s;
+
+    public MyThread(Socket s) {
+        this.s = s;
+    }
+
+    @Override
+    public void run() {
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            PrintWriter out = new PrintWriter(s.getOutputStream(), true);
+
+            String request = in.readLine();
+
+            System.out.println("RICHIESTA: "+request);
+            String header="";
+
+            do {
+                header = in.readLine();
+                System.out.println(header);
+            } while (!header.isEmpty());
+
+            String response = "<strong>hello</strong> world";
+            String error = "Error 404 not found";
+
+            if (request.equals("GET /ciao HTTP/1.1")) {
+                out.println(
+                    "HTTP/1.1 200 OK\r\n"
+                  + "Content-Length: " 
+                  + response.length()
+                  + "\r\nContent-Type: text/html\r\n" 
+                  + "\r\n" + response
+              );
+            }else{
+                out.println(
+                    "HTTP/1.1 404 Not Found\r\n"
+                  + "Content-Length: " 
+                  + error.length()
+                  + "\r\nContent-Type: text/html\r\n" 
+                  + "\r\n" + error
+              );
+            }
+            
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
